7615e531cf1792e95bd75547f8aa124e53a027f4
Nocenthalt <40654580+Nocenthalt@users.noreply.github.com>
2021-06-09 22:46:52 +0700

    Delete src directory

--- src/Routes/router.js
+++
@@ -1,9 +0,0 @@
-import Main from './main'
-import Map from './map'
-import React from 'react'
-const routes = {
-    "/": () => <Main />, 
-    "/map": () => <Map />
-  };
-
-  export default routes;
